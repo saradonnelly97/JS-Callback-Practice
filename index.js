8a2446fdf3b7a59f0f2edd383fa081b8e49a19a3
saradonnelly97 <sara.donnelly8@gmail.com>
2023-05-15 19:56:09 -0400

    Revisit.

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-
 const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250;
 
-function moveCharacter(){ 
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = `assets/green-character/static.gif`
+    }
     if(direction === 'west'){
-        x = x - 1
+        character.src = `assets/green-character/west.gif`
     }
     if(direction === 'north'){
-        y = y + 1
+        character.src = `assets/green-character/north.gif`
     }
     if(direction === 'east'){
-        x = x + 1
+        character.src = `assets/green-character/east.gif`
     }
     if(direction === 'south'){
-        y = y - 1
+        character.src = `assets/green-character/south.gif`
     }
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
 }
-setInterval(moveCharacter, 1)                                    
 
-
-
-move(character).to(100, 250)
-
-
-
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 
 
